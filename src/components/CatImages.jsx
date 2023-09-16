8e0e62047b07d9ca9a6b3ed1a3768686e8ef5668
Jhonier Jimenez <jhonier@experimentality.co>
2023-09-16 12:03:46 -0500

    initial request

---
+++ src/components/CatImages.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+function CatImages() {
+  const [catImages, setCatImages] = useState([]);
+
+  async function fetchCatImages() {
+    try {
+      const response = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=10"
+      );
+      const data = await response.json();
+      setCatImages(data);
+    } catch (error) {
+      console.error("Error fetching cat images:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCatImages();
+  }, []);
+
+  return (
+    <div className="cat-images">
+      {catImages.map((catImage) => (
+        <img
+          key={catImage.id}
+          src={catImage.url}
+          alt={`Cat ${catImage.id}`}
+          width={catImage.width}
+          height={catImage.height}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CatImages;
